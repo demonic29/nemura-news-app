b0164505f4077dec5b997990cc2bed2949141b2c
- Alex - <130956307+demonic29@users.noreply.github.com>
2026-02-14 21:04:07 +0900

    Set dynamic response to 'force-dynamic'

--- app/api/hatena/route.ts
+++ app/api/hatena/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { parseStringPromise } from "xml2js";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: Request) {
     try {
         
